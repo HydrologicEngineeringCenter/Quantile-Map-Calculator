5933733c9cfaaf1599b073a6c4b43a20ed18ec43
Brennan Beam <BrennanBeam@gmail.com>
2023-10-30 15:42:32 -0700

    Create simple histogram

---
+++ AEPGG.Model/Histogram.cs
@@ -0,0 +1,20 @@
+﻿namespace AEPGG.Model
+{
+    public class Histogram
+    {
+        public int[] BinCounts { get; } 
+        public float BinInterval { get; }
+        public float Min { get; }
+        public Histogram(float binInterval, float min, int numBins)
+        {
+            BinCounts = new int[numBins];
+            BinInterval= binInterval;
+            Min = min;
+        }
+        public void Add(float value)
+        {
+            int binIndex = (int)Math.Ceiling((value - Min) / BinInterval);
+            BinCounts[binIndex]++;
+        }
+    }
+}
