f7776e6203fd676b258219049e06d0937d94fec8
Brennan Beam <BrennanBeam@gmail.com>
2023-10-30 16:11:41 -0700

    Add InverseCDF Method

--- AEPGG.Model/Histogram.cs
+++ AEPGG.Model/Histogram.cs
@@ -3,18 +3,33 @@
     public class Histogram
     {
         public int[] BinCounts { get; } 
-        public float BinInterval { get; }
+        public float BinWidth { get; }
         public float Min { get; }
-        public Histogram(float binInterval, float min, int numBins)
+        public int NumSamples { get; }
+        public Histogram(float binWidth, float min, int numBins)
         {
             BinCounts = new int[numBins];
-            BinInterval= binInterval;
+            BinWidth= binWidth;
             Min = min;
         }
         public void Add(float value)
         {
-            int binIndex = (int)Math.Ceiling((value - Min) / BinInterval);
+            int binIndex = (int)Math.Ceiling((value - Min) / BinWidth);
             BinCounts[binIndex]++;
         }
+        public float InverseCDF(float probability)
+        {
+            int binIndex = 0;
+            float binStartingPercentile = 0;
+            int numSamplesSoFar = 0;
+            while(binStartingPercentile > probability)
+            {
+                numSamplesSoFar += BinCounts[binIndex];
+                binStartingPercentile = 1 - (numSamplesSoFar/BinCounts.Length);
+                binIndex++;
+            }
+            return (binIndex * BinWidth) + (BinWidth/2) ;
+        }
+
     }
 }
