8f2365eedc84b11597c3822c44b51382a6b6d611
Brennan Beam <BrennanBeam@gmail.com>
2023-10-31 11:51:43 -0700

    bug fixes.

--- AEPGG.Model/Histogram.cs
+++ AEPGG.Model/Histogram.cs
@@ -2,34 +2,34 @@
 {
     public class Histogram
     {
-        public int[] BinCounts { get; } 
+        public int[] BinCounts { get; }
         public float BinWidth { get; }
         public float Min { get; }
-        public int NumSamples { get; }
+        public int NumSamples { get; set; }//We may just know this apriori
         public Histogram(float binWidth, float min, int numBins)
         {
             BinCounts = new int[numBins];
-            BinWidth= binWidth;
+            BinWidth = binWidth;
             Min = min;
         }
         public void Add(float value)
         {
             int binIndex = (int)Math.Ceiling((value - Min) / BinWidth);
             BinCounts[binIndex]++;
+            NumSamples++; //We may just know this apriori
         }
-        public float InverseCDF(float probability)
+        public float InverseCDF(float exceedenceProbability)
         {
             int binIndex = 0;
             float binStartingPercentile = 0;
             int numSamplesSoFar = 0;
-            while(binStartingPercentile > probability)
+            while (binStartingPercentile > exceedenceProbability)
             {
                 numSamplesSoFar += BinCounts[binIndex];
-                binStartingPercentile = 1 - (numSamplesSoFar/BinCounts.Length);
+                binStartingPercentile = 1 - (numSamplesSoFar / BinCounts.Length);
                 binIndex++;
             }
-            return (binIndex * BinWidth) + (BinWidth/2) ;
+            return (binIndex * BinWidth) + (BinWidth / 2);
         }
-
     }
 }
