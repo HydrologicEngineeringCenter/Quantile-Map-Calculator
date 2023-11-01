cff91549f106a9cb8fd2a3c9f60256703f0efa80
Brennan Beam <BrennanBeam@gmail.com>
2023-11-01 13:21:45 -0700

    add ras as submodule.

---
+++ AEPGG.Model/RasHelper.cs
@@ -0,0 +1,17 @@
+﻿
+using Amazon.S3.Model;
+using RasMapperLib;
+using Ras.
+
+namespace AEPGG.Model
+{
+    public static class RasHelper
+    {
+       public static float[] GetWSEsForAllNodes()
+        {
+            //Add logic for getting WSE 
+            Ras.Layers.ResultsDatsets.
+            return Array.Empty<float>();
+        }
+    }
+}
