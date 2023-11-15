909761aa1ce5b4fc53a41e546b1ed0637bf986d8
Brennan Beam <BrennanBeam@gmail.com>
2023-11-14 16:01:39 -0800

    set up program to test on real data.

--- AEPGG.Model/Program.cs
+++ AEPGG.Model/Program.cs
@@ -1,5 +1,15 @@
 ﻿using AEPGG.Model;
- static void main(string[] args)
+
+string lifecycleDirectoryPath = "D:\\AEP Grid\\Muncie_WAT\\runs\\Without_Project_Conditions\\FRA_50yr\\realization 1\\lifecycle 1\\";
+string rasFilePath = "\\RAS\\Muncie.p08.hdf";
+float[] theAEPs = new float[] { .99f, .5f, .2f, .1f, .02f };
+
+Project proj = new("", theAEPs, .5f, 20);
+string[] eventdirs = Directory.GetDirectories(lifecycleDirectoryPath);
+
+for (int i = 0; i < eventdirs.Length; i++)
 {
-    Console.WriteLine("poo");
+    proj.AddResults(new RasResultWrapper(eventdirs[i] + rasFilePath));
 }
+Console.WriteLine("poopy");
+Console.ReadLine();
