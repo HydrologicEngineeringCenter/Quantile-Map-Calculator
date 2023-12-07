1cde6cfb7bcbcf1693e57257e4c31e40d063f787
Brennan Beam <BrennanBeam@gmail.com>
2023-12-07 15:56:57 -0800

    implement multiple 2D Areas

--- AEPGG.Model/Project.cs
+++ AEPGG.Model/Project.cs
@@ -4,11 +4,11 @@ namespace AEPGG.Model
 {
     public class Project
     {
-        public string OutputFilePath { get;}
-        public float[] Probabilities { get;}
-        public Histogram[] Histograms { get; private set;} = Array.Empty<Histogram>();//Will need to do jagged array if we have multiple 2D areas later. 
+        public string OutputFilePath { get; }
+        public float[] Probabilities { get; }
+        public Histogram[][] Histograms { get; private set; }
         public float BinWidth { get; }
-        public float Range { get; }//This is the expected range of depths/vel/etc for a given cell.
+        public float Range { get; }//This is the expected range of WSE for a given cell.
         public Project(string outputFilePath, float[] probabilities, float binWidth, float range)
         {
             OutputFilePath = outputFilePath;
@@ -16,67 +16,60 @@ namespace AEPGG.Model
             Range = range;
             BinWidth = binWidth;
         }
-        
-        public void AddResults(IHydraulicResults result) 
+
+        public void AddResults(IHydraulicResults result)
         {
-            if (Histograms.Length == 0)
-            { // Then this is the first result we are adding, and it's a real result with a file.
+            if (Histograms == null) // Then this is the first result we are adding, and it's a real result with a file.
+            {
                 if (File.Exists(result.FilePath))
                 {
                     File.Copy(result.FilePath, OutputFilePath, true); // copy the first result over to the output directory. We'll use this as the starting point for our output file. 
-                } 
+                }
                 InitializeHistograms(result);
             }
             float[][] data = result.Max2DWSEs;
-            for (int i = 0; i < data[0].Length; i++)//hard coding 1 2D area for now.
+            for (int i = 0; i < data.Length; i++)//for each 2D area
             {
-                Histograms![i].Add(data[0][i]);//hard coding 1 2D area for now.
+                for (int j = 0; j < data[i].Length; j++) //for each cell in the 2D area
+                {
+                    Histograms[i][j].Add(data[i][j]);
+                }
             }
         }
+
         private void InitializeHistograms(IHydraulicResults result)
         {
             float[][] data = result.Min2DWSEs;
-            Histograms = new Histogram[data[0].Length];//hard coding 1 2D area for now.
-            for (int i = 0; i < data[0].Length; i++) //hard coding 1 2D area for now.
+            Histograms = new Histogram[data.Length][];
+            for (int i = 0; i < data.Length; i++) //for each 2D area
             {
-                float min = data[0][i];//hard coding 1 2D area for now.
-                float max = data[0][i] + Range;//hard coding 1 2D area for now.
-                Histograms[i] = new(BinWidth, min, max);
+                Histogram[] specificAreaHistograms = new Histogram[data[i].Length];
+                for (int j = 0; j < data[i].Length; j++) //for each cell in the 2D area
+                {
+                    float min = data[i][j];
+                    float max = min + Range;
+                    specificAreaHistograms[j] = new(BinWidth, min, max);
+                }
+                Histograms[i] = specificAreaHistograms;
             }
         }
-        private float[] GetResultsForAEP(float aep)
+        private float[][] GetResultsForAEP(float aep)
         {
-            float[] resultsForAEP = new float[Histograms.Length]; //1 x 2d area
-            for (int i = 0; i < Histograms.Length; i++) //1 x 2d area
+            float[][] resultsForAEP = new float[Histograms.Length][]; 
+            for (int i = 0; i < Histograms.Length; i++) //for each 2D area
             {
-                resultsForAEP[i] = Histograms[i].InverseCDF(aep);
+                resultsForAEP[i] = new float[Histograms[i].Length];
+                for (int j = 0; j < Histograms[i].Length; j++) //for each cell in the 2D area
+                {
+                    resultsForAEP[i][j] = Histograms[i][j].InverseCDF(aep);
+                }
             }
             return resultsForAEP;
         }
         public void SaveResults()
         {
-            float[] result = GetResultsForAEP(Probabilities[^1]); //only using 1 AEP for now. 
-            DebugReport(result);
-            RasTools.OverwriteMaxWSEForAll2DCells(OutputFilePath, result);
-        }
-
-        private void DebugReport(float[] newMax)
-        {
-            float[] originalResults = RasTools.GetMaxOrMinWSEForAll2DCells(OutputFilePath, true)[0];
-            float[] Mins = RasTools.GetMaxOrMinWSEForAll2DCells(OutputFilePath, false)[0];
-            for (int i = 0; i < originalResults.Length; i++)
-            {
-                if (originalResults[i] != newMax[i])
-                {
-                    Console.WriteLine($" There was a change at index:{i}! OG: {originalResults[i]} New: {newMax[i]}");
-                }
-                if (newMax[i] < Mins[i])
-                {
-                    Console.WriteLine($" There's inundation at index:{i}! Min: {Mins[i]} New: {newMax[i]}");
-                }
-                
-            }
-
+            float[][] result = GetResultsForAEP(Probabilities[^1]); //only using 1 AEP for now. 
+            RasTools.OverwriteMaxWSEForAll2DCells(OutputFilePath, result[0]);//only writing to one 2D area for now.
         }
     }
 }
--- AEPGG.ModelTest/ProjectShould.cs
+++ AEPGG.ModelTest/ProjectShould.cs
@@ -6,14 +6,14 @@ namespace AEPGG.ModelTest
     public class ProjectShould
     {
         public const string outFilePath = @".\output.txt";
-        public float[] theAEPs = new float[] { .99f, .5f, .2f, .1f, .02f, .01f, .005f, .002f };
+        public float[] theAEPs = [.99f, .5f, .2f, .1f, .02f, .01f, .005f, .002f];
 
         [Fact]
         public void AddProperly()
         {
             Project project = new(outFilePath, theAEPs, 0.5f, 100);
             project.AddResults(GetMockResults(1)[0]);
-            Assert.Equal(1, project.Histograms[0].NumSamples); //We have a sample
+            Assert.Equal(1, project.Histograms[0][0].NumSamples); //We have a sample
         }
 
         [Fact]
@@ -26,9 +26,9 @@ namespace AEPGG.ModelTest
             {
                 project.AddResults(mockResult);
             }
-            Assert.Equal(5, project.Histograms.Length);
-            Assert.Equal(project.Histograms[0].NumSamples, numPoints);
-            float valueOfThe100yr = project.Histograms[0].InverseCDF(.01f);
+            Assert.Equal(5, project.Histograms[0].Length);
+            Assert.Equal(project.Histograms[0][0].NumSamples, numPoints);
+            float valueOfThe100yr = project.Histograms[0][0].InverseCDF(.01f);
             Assert.True(valueOfThe100yr > 5);
         }
 
