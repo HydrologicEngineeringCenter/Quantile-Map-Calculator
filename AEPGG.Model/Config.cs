2a8ce2f4f9fc748be56d3cedc88ec5fe17eb7fad
Brennan Beam <BrennanBeam@gmail.com>
2024-05-22 10:53:13 -0700

    formalize JSON and organize entry point

--- AEPGG.Model/Config.cs
+++ AEPGG.Model/Config.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 namespace AEPGG.Model;
@@ -8,34 +8,39 @@ public class Config
     /// <summary>
     /// Realization computes aggregate the Max WSEs. Project Level computes need to access the timestep data for each saved AEP from previous realization computes. 
     /// </summary>
+    [JsonPropertyName("isRealizationCompute")]
     public bool IsRealizationCompute { get; set; }
     /// <summary>
     /// Directory where the results are stored. All .hdf files will be scraped and added to the results histograms. Includes subdirectories. 
     /// </summary>
+    [JsonPropertyName("resultsDirectory")]
     public string ResultsDirectory { get; set; }
     /// <summary>
     /// Path and filename for the output file.
     /// </summary>
+    [JsonPropertyName("outputPath")]
     public string OutputPath { get; set; }
     /// <summary>
     /// The AEPs which will be pulled from the results histograms. 
     /// </summary>
+    [JsonPropertyName("desiredAeps")]
     public float[] DesiredAEPs { get; set; }
     /// <summary>
     /// Histogram bin width.
     /// </summary>
+    [JsonPropertyName("binWidth")]
     public float BinWidth { get; set; }
     /// <summary>
     /// Maximum range of WSEs we expect our histograms to capture. This is ~= the maximum depth we expect to see. It determines number of bins.
     /// </summary>
+    [JsonPropertyName("range")]
     public float Range { get; set; }
 
     /// <summary>
     /// Holds all necessary information for a compute. 
     /// </summary>
-    internal Config()
+    public Config()
     {
-        //Internal because I want people outside deserielizing this object from JSON.
     }
     /// <summary>
     /// Copies the first results file to the output file, Creates histograms of all results files which exist recursively in the ResultsDirectory, and writes the results to the output file at the locations requested.
--- AEPGG.Model/Program.cs
+++ AEPGG.Model/Program.cs
@@ -1,21 +1,42 @@
 ﻿using AEPGG.Model;
+using System.Text.Json;
 
-//Hard coded to local data. too big to upload to github. 
-//Do this as JSON for acutal computes. 
-string lifecycleDirectoryPath = @"D:\AEP Grid\All2DMuncie\Muncie_WAT\runs\Without_Project_Conditions\FRA_50yr\realization 1\lifecycle 1\";
-string rasFilePath = "RAS\\Muncie.p13.hdf";
-string outputFilePath = "D:\\AEP Grid\\muncieAll2D_50_WriteMultipleAEPS.hdf";
-float[] theAEPs = [.99f, .5f, .2f, .1f, .02f];
-Config config = new()
+string jsonString = ScriptSetup();
+EntryPoint(jsonString);
+
+//This is just creating a JSON string to pass into the entry point. In a real compute, this string would be written by the user outside the library. 
+static string ScriptSetup()
 {
-    ResultsDirectory = lifecycleDirectoryPath,
-    OutputPath = outputFilePath,
-    DesiredAEPs = theAEPs,
-    IsRealizationCompute = true,
-    BinWidth = 0.1f,
-    Range = 20f
-};
-config.Compute();
-Console.WriteLine("Done");
+    //Hard coded to local data. too big to upload to github. 
+    //Do this as JSON for acutal computes. 
+    string lifecycleDirectoryPath = @"D:\AEP Grid\All2DMuncie\Muncie_WAT\runs\Without_Project_Conditions\FRA_50yr\realization 1\lifecycle 1\";
+    string outputFilePath = "D:\\AEP Grid\\muncieAll2D_50_WriteMultipleAEPS.hdf";
+    float[] theAEPs = [.99f, .5f, .2f, .1f, .02f];
+    Config config = new()
+    {
+        ResultsDirectory = lifecycleDirectoryPath,
+        OutputPath = outputFilePath,
+        DesiredAEPs = theAEPs,
+        IsRealizationCompute = true,
+        BinWidth = 0.1f,
+        Range = 20f
+    };
+    string jsonString = JsonSerializer.Serialize(config);
+    return jsonString;
+}
 
-
+//Imagining this would be the whole Program file entry point if this were published as a console app. 
+static void EntryPoint(string jsonString)
+{
+    Config jsonConfig;
+    try
+    {
+        jsonConfig = JsonSerializer.Deserialize<Config>(jsonString);
+    }
+    catch (JsonException e)
+    {
+        Console.WriteLine("Failed to deserialize");
+        return;
+    }
+    jsonConfig.Compute();
+}
