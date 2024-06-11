17c78004eb45306d70563ffd32b01f0cfd8df78a
Brennan Beam <BrennanBeam@gmail.com>
2024-06-11 16:00:48 -0700

    comments

--- AEPGG.Model/RasResultWrapper.cs
+++ AEPGG.Model/RasResultWrapper.cs
@@ -11,10 +11,12 @@ public class RasResultWrapper : IHydraulicResults
     }
 
     //not making these properties because I don't want to query the mesh names for every result file. They'll all be the same. Want to hand in mesh names.  
+    /// <returns>[MeshIndex][CellIndex]</returns>
     public float[][] GetMax2DWSE(string[] meshNames )
     {
         return RasTools.H5ReaderTools.GetMaxOrMinWSEForAll2DCells(FilePath, true, meshNames);
     }
+    /// <returns>[MeshIndex][CellIndex]</returns>
     public float[][] GetMin2DWSE(string[] meshNames )
     {
         return RasTools.H5ReaderTools.GetMaxOrMinWSEForAll2DCells(FilePath, false, meshNames);
@@ -27,12 +29,17 @@ public class RasResultWrapper : IHydraulicResults
     {
         return RasTools.H5ReaderTools.GetMinWSEForAllXS(FilePath);
     }
-
+    /// <summary>
+    /// Gets the water surface elevations for a specific profile in the results timeseries. 
+    /// </summary>
     public float[] GetXSWSE(int profileIndex)
     {
         return RasTools.H5ReaderTools.GetWSEForXSProfile(FilePath,profileIndex);
     }
-    /// <returns>A jagged array which contains the 2D WSE cell elevations [MeshIndex][CellIndex]</returns>
+    /// <summary>
+    /// Gets the water surface elevations for a specific profile in the results timeseries.
+    /// </summary>
+    /// <returns>[MeshIndex][CellIndex]</returns>
     public float[][] Get2DWSE(int profileIndex, string[] meshNames)
     {
         float[][] results = new float[meshNames.Length][];
